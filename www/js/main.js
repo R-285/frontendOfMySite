a4413670693da4c43a2a854fd42a1726459ea1ca
Alexander Anikeev <Alex-anikeev.1998@yandex.ru>
2021-06-02 15:01:20 +0300

    js. Not for all pages

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,12 @@
 new Vue({
-    el: '#log_reg_zone',
+    el: '#page-wrap',
     data: {
         showAuth: true,
+        login: "",
+        password: "",
+        name: "",
+        eMail: "",
+        phone: "",
     },
     methods: {
         changeTrue () {
@@ -9,6 +14,42 @@ new Vue({
         },
         changeFalse () {
             this.showAuth = false
+        },
+        logInAcc() {
+            if ((this.login == "") || (this.password == "")) {
+                alert("Для входа в свой аккаунт все поля должны быть заполнены");
+            } else {
+
+                fetch('https://jsonplaceholder.typicode.com/posts/1', {
+                    method: 'PUT',
+                    body: JSON.stringify({
+                        login: this.login,
+                        password: this.password
+                    }),
+                    headers: {
+                        'Content-type': 'application/json; charset=UTF-8',
+                    },
+                })
+                    .then((response) => response.json());
+            }
+        },
+        regNewAcc(){
+            if ((this.name == "") || (this.eMail == "") || (this.password == "")) {
+                alert("Для регистрации все поля должны быть заполнены");
+            } else {
+                fetch('https://jsonplaceholder.typicode.com/posts/1', {
+                    method: 'PUT',
+                    body: JSON.stringify({
+                        name: this.name,
+                        eMail: this.eMail,
+                        phone: this.phone,
+                    }),
+                    headers: {
+                        'Content-type': 'application/json; charset=UTF-8',
+                    },
+                })
+                    .then((response) => response.json());
+            }
         }
     }
 })
